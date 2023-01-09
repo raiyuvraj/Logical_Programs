f46e0b2b58e1449a4c2f4864fc71167e3338a766
raiyuvraj <yuvirai017@gmail.com>
2023-01-09 06:43:36 +0530

    [Yuvraj]-Fibonacci_Series

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void FibbSeries()
+        {
+            Console.WriteLine("Enter length of Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int firstnumber = 0, secondnumber = 1, result = 0;
+            Console.WriteLine(firstnumber +"\n"+ secondnumber);
+
+            for(int i = 0; i < length; i++)
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Logical Programs");
+                Console.WriteLine("1: Fibonacci Series" + "\n"+ "2: Exit");
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                switch(num)
+                {
+                    case 1:
+                        FibonacciSeries fs = new FibonacciSeries();
+                        fs.FibbSeries();
+                        break;
+                    default: break;
+                            flag = false;
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
